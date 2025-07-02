1d300e030e450674401ad0f92bd8288405b1517b
foxsportscomactivate <radhjain1@gmail.com>
2025-07-02 15:45:39 +0530

    Update conf.py

--- conf.py
+++ conf.py
@@ -16,7 +16,7 @@ import sys
 
 # -- Project information -----------------------------------------------------
 
-project = 'HP Smart'
+project = 'Brother Printer Installation'
 
 
 # The full version, including alpha/beta/rc tags
